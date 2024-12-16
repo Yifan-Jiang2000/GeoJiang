3e4b6b1a5e2cce702238736d67ae45f4db97838c
Yifan-Jiang2000 <yifan.jiang2000@gmail.com>
2024-12-16 09:32:04 +0100

    fix tolerance bug

--- src/main.cpp
+++ src/main.cpp
@@ -24,108 +24,6 @@ std::string trim(const std::string& str) {
 
 int main()
 {
-    // double temp[16] = {1, 2, 3, 4, 2, 0, 5, 6, 3, 5, 0, 7, 4, 6, 7, 0};
-    // double temp[16] = {4, 1, 2, 3, 1, 3, 1, 2, 2, 1, 2, 1, 3, 2, 1, 1};
-    // double temp[16] = {2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2};
-    // double temp[16] = {2, 1, 1, 6, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2};
-    // Matrix sym(10,10);
-    // for (size_t i = 0; i < sym._row; ++i) {
-    //     for (size_t j = i; j < sym._column; ++j) {
-    //         sym(i, j) = rand() / 2147483647.0;
-    //         sym(j, i) = sym(i, j);
-    //     }
-    // }
-    // sym.print();
-    // long i = 10;
-    // long nb = 5;
-    // double E[9];
-    // double TAU[9];
-    // double W[200];
-    // long LDW = 40;
-    // dlatrd_("U", &i, &nb, sym._Mdata, &i, E, TAU, W, &LDW);
-    // sym.print();
-
-    // Matrix a = Matrix(500,500);
-    // Matrix b = Matrix(500,500);
-    // for (size_t i = 0; i < a._row * a._column; ++i)
-    // {
-    //     a._Mdata[i] = rand();
-    //     b._Mdata[i] = rand();
-    // }
-    // Matrix c = Matrix(500,500);
-    
-    // std::clock_t start = clock();
-    // c = a * b;
-    // // c.print();
-    // // long i = 500;
-    // // double alpha = 1.;
-    // // double beta = 0.;
-    // // dgemm_("N", "N", &i, &i, &i, &alpha, a._Mdata, &i, b._Mdata, &i, &beta, c._Mdata, &i);
-    
-    // std::clock_t end = clock();
-    // std::cout << (double)(end - start) / CLOCKS_PER_SEC << std::endl;
-
-
-    // Matrix m = Matrix(4, 4);
-
-    // for (int i = 0; i < 4; ++i)
-    // {
-    //     for (int j = 0; j < 4; ++j)
-    //     {
-    //         m._Mdata[i * m._column + j] = temp[i * m._column + j];
-    //     }
-    // }
-
-    // Matrix hessen(m._row,m._column);
-    // Matrix trans(m._row,m._column);
-    // m.getUpHessenberg(trans,hessen);
-    // m.print();
-    // hessen.print();
-    // std::cout << m._Mdata << "  " << hessen._Mdata << std::endl;
-    // Matrix Q(m._row,m._column);
-    // Matrix R(m._row,m._column);
-
-    // Matrix lambda(m._row,m._column);
-    // Matrix v(m._row,m._column);
-    // Matrix invM = m.getInv();
-    // m.eigenDecomposition(lambda,v);
-    // m.print();
-    // lambda.print();
-    // v.print();
-
-    // (v.transpose() * m * v).print();
-    // Matrix invLambda = lambda.getInvDiag();
-    // Matrix invM = v * invLambda * v.transpose();
-    // (invM * m).print();
-    
-
-    // std::cout << m.det() << std::endl;
-
-    // m.QRDecomposition(Q, R);
-    // m.print();
-    // Q.print();
-    // R.print();
-
-    // Matrix a(m);
-    // a = m.guassEliminaton();
-    // m.print();
-    // a.print();
-    // Matrix eigenVec(m._row, 1);
-    // double lambda = m.powerIter(eigenVec);
-    // m.print();
-    // std::cout << m.det() << std::endl;
-    // eigenVec.print();
-    // std::cout << lambda << std::endl;
-
-    // m.print();
-    // std::cout << m.det() << std::endl;
-    // m.transpose().print();
-    // m.inverse().print();
-    // return 1;
-
-
-
-
 
     std::unordered_map<std::string, std::string> keywords;
     FILE *set = fopen("setting.ini", "r");
@@ -159,7 +57,7 @@ int main()
     if (keywords["eigenSolver"] == "jiang") eigenSolver = &Matrix::eigenDecomposition;
     else eigenSolver = &Matrix::eigenDecompositionLAPACK;
 
-    if (keywords.find("tolerance") != keywords.end()) tol = 0.001;
+    if (keywords.find("tolerance") == keywords.end()) tol = 0.001;
     else tol = std::stod(keywords["tolerance"]);
 
 
